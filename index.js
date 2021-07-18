b82eccefb007d2a3c85674a9a2f3d7be4430ce23
nicolas <novarinomatias55@gmail.com>
2021-07-17 23:02:05 -0300

    4ta pre entrega

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 
-window.sr = ScrollReveal();
+/* window.sr = ScrollReveal();
 
     sr.reveal('.banner, .carousel, #menu',{
         duration: 1000,
         origin: 'top',
         distance: '100px',
         reset: 'true',
-        /* delay: 100, */
+        
     })
 
     sr.reveal('#menu',{
         origin: 'left',
         duration: 1600,
-   
+        reset: 'true',
     })
 
     sr.reveal('.carousel',{
         origin: 'right',
         duration: 1400,
+        reset: 'true',
     })  
     
     sr.reveal('.card1, .card3',{
@@ -34,3 +35,4 @@ window.sr = ScrollReveal();
         reset: 'true',
         
     }) 
+ */
